ed84b77f012b1af76c8dd88ca7b33de259fbccbc
Sakshamsaks <saksham.agrawal.588@gmail.com>
2024-06-26 22:19:46 +0530

    TMDB API data fetch and build Main Container

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,10 @@
-import { signOut } from "firebase/auth";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 import { auth } from "../utils/firebase";
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { addUser, removeUser } from "../utils/userSlice";
+import { LOGO } from "../utils/constants";
 
 const Header=()=>{
 
@@ -9,19 +12,36 @@ const Header=()=>{
 
     const user=useSelector(Store=>Store.user);
 
+    const dispatch=useDispatch();
+
     const handleSignOut=()=>{
-        signOut(auth).then(() => {
-            navigate("/");
-          }).catch((error) => {
+        signOut(auth).then(() => {})
+        .catch((error) => {
             navigate("/error");
           });
     };
+
+    useEffect(()=>{
+        const unsubscribe=onAuthStateChanged(auth, (user) => {
+            if (user) {
+                const {uid, email, displayName, photoURL} = user;
+                dispatch(addUser({uid:uid, email:email, displayName:displayName, photoURL:photoURL}));
+                navigate("/browse");
+            } 
+            else {
+                dispatch(removeUser());
+                navigate("/");
+            }
+        });
+
+        return ()=> unsubscribe();
+    },[]);
 
     return (
         <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
             <img 
                 className="w-40"
-                src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+                src={LOGO}
                 alt="logo"
             />
             {user && <div className="flex p-2">
